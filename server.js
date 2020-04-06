4e194b197ac5cefd3d0e6282015246690d059000
Sunil Karki <karkisunil1200@gmail.com>
2020-04-06 16:33:53 -0500

    Done with the MPV

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const server = express();
 
-const hubs = [
+const users = [
   {
     id: 1,
     name: 'Jane Doe',
@@ -17,13 +17,25 @@ server.get('/', (req, res) => {
 });
 
 server.get('/api/users', (req, res) => {
-  res.json(hubs);
+  res.json(users);
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  const user = users.find(user => user.id == id);
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({message: 'user not found'});
+  }
 });
 
 server.post('/api/users', (req, res) => {
   const user = req.body;
-  hubs.push(user);
-  res.status(201).json(hubs);
+  users.push(user);
+  res.status(201).json(users);
 });
 
 module.exports = server;
